27525c45a12eb215ac30505478f50f934472311e
Dominic <domqi1111@gmail.com>
2025-05-10 22:29:46 +1000

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Dominic  Qi on 10/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
